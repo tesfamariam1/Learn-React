f5edf552fa5cbc3bd9c89da89750771091cdd359
Tesfamariam Teshome <tesfumerry16@gmail.com>
2023-12-07 17:28:00 +0300

    Making use of state

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import AppClass from './components/AppClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    // <App />
+    <AppClass />
   </React.StrictMode>
 );
 
