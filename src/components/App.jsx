49d22c28b065d5eb192d31a34ca42992d16b2515
Tesfamariam Teshome <tesfumerry16@gmail.com>
2023-12-08 16:46:44 +0300

    Editting todos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,21 +8,25 @@ function App() {
       id: 1,
       title: 'Finish React Series',
       isComplete: false,
+      isEditing: false,
     },
     {
       id: 2,
       title: 'Go to Home',
       isComplete: false,
+      isEditing: false,
     },
     {
       id: 3,
       title: 'Go to Grocery',
       isComplete: false,
+      isEditing: false,
     },
     {
       id: 4,
       title: 'Do other things',
       isComplete: false,
+      isEditing: false,
     },
   ]);
   const [todoInput, setTodoInput] = useState('');
@@ -47,6 +51,51 @@ function App() {
   function deleteTodo(id) {
     setTodos([...todos].filter(todo => todo.id !== id));
   }
+  function completeTodo(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+  function markAsEditing(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = true;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+  function updateTodo(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        if (event.target.value.trim().length === 0) {
+          todo.isEditing = false;
+          return todo;
+        }
+        todo.title = event.target.value;
+        todo.isEditing = false;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+  function cancelEdit(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = false;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
   function handleInput(event) {
     setTodoInput(event.target.value);
   }
@@ -68,8 +117,37 @@ function App() {
           {todos.map((todo, index) => (
             <li key={todo.id} className="todo-item-container">
               <div className="todo-item">
-                <input type="checkbox" />
-                <span className="todo-item-label">{todo.title}</span>
+                <input
+                  type="checkbox"
+                  onChange={() => completeTodo(todo.id)}
+                  checked={todo.isComplete ? true : false}
+                />
+                {!todo.isEditing ? (
+                  <span
+                    onDoubleClick={() => markAsEditing(todo.id)}
+                    className={`todo-item-label ${
+                      todo.isComplete ? 'line-through' : ''
+                    }`}
+                  >
+                    {todo.title}
+                  </span>
+                ) : (
+                  <input
+                    type="text"
+                    onBlur={event => updateTodo(event, todo.id)}
+                    onKeyDown={event => {
+                      if (event.key === 'Enter') {
+                        updateTodo(event, todo.id);
+                      } else if (event.key === 'Escape') {
+                        cancelEdit(event, todo.id);
+                      }
+                    }}
+                    className="todo-item-input"
+                    defaultValue={todo.title}
+                    autoFocus
+                  />
+                )}
+
                 {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
               </div>
               <button onClick={() => deleteTodo(todo.id)} className="x-button">
